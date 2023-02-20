65863397c46792d8f6826acb7e2c675082e3fb20
HJKIM <guswns589898@gmail.com>
2023-02-20 20:20:32 +0900

    230220 : Log 기능 추가 예정

--- AutoStockTrading/Reference/CommonFunction.cs
+++ AutoStockTrading/Reference/CommonFunction.cs
@@ -8,5 +8,27 @@ namespace AutoStockTrading.Reference
 {
     class CommonFunction
     {
+
+        private static CommonFunction instance;
+        public static CommonFunction Global
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new CommonFunction();
+                return instance;
+            }
+        }
+
+        // Forms =============================================
+        public form_Main frm_Main;
+        // ===================================================
+
+        // Class =============================================
+        public CommonCommunication  Comm;
+        public CommonLog            Log;
+        // ===================================================
+
+
     }
 }
--- AutoStockTrading/Reference/CommonLog.cs
+++ AutoStockTrading/Reference/CommonLog.cs
@@ -2,11 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AutoStockTrading.Reference
 {
     class CommonLog
     {
+        private List<string> _logList;
+
+        Task taskLog;
+        object _Lock;
+
+        bool m_StopTaskLog;
+        bool m_Disposed;
+        bool m_StartLogging;
+        string m_StartupPath;
+
+        public CustomLog(string StartupPath)
+        {
+            _logList = new List<string>();
+
+            m_StopTaskLog   = false;
+            m_Disposed      = false;
+            m_StartLogging  = false;
+
+            m_StartupPath  = StartupPath;
+
+            _Lock = new object();
+
+            taskLog = Task.Run(() => Logging());
+        }
+
+        private void Logging()
+        {
+            while(m_StopTaskLog == false)
+            {
+                if(!m_StartLogging)
+                {
+                    Thread.Sleep(10);
+                    continue;
+                }
+
+                // File에다가 Log 심어넣어버리기
+                LoggingToFile();
+
+                Thread.Sleep(10);
+            }
+        }
     }
 }
